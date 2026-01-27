970f0ba544baa36c5096fb24dbaaef02fee83959
Mr Freaky <mf@auav.ui>
2026-01-27 16:58:54 +0700

    added more length units to converter

--- src/Length.cpp
+++ src/Length.cpp
@@ -5,7 +5,11 @@ Length::Length() {
     // base = meter
     multipliers["m"] = 1.0;
     multipliers["km"] = 1000.0;
+    multipliers["cm"] = 0.01;
+    multipliers["mm"] = 0.001;
+    multipliers["mi"] = 1609.34;
     multipliers["ft"] = 0.3048;
+    multipliers["in"] = 0.0254;
 }
 
 double Length::toBase(double value, std::string unit){
